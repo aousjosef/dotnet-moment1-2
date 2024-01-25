bac0626995eb7be471db3beaa8614e36a5cd89f7
aousjosef <aoussabah@gmail.com>
2024-01-25 04:24:34 +0100

    completed fetch and created  shared component for fetch

---
+++ UserDataClass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Text.Json.Serialization;
+
+
+namespace UserData;
+
+public class UserDataClass
+{
+    public string? Code { get; set; }
+    public string? CourseName { get; set; }
+    public string? Progression { get; set; }
+    public string?  Syllabus { get; set; }
+}
+
+
+
+
+
--- obj/Debug/net8.0/12.AssemblyInfo.cs
+++ obj/Debug/net8.0/12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52222f2a0f3c037febfbdcb5902e74b383894025")]
 [assembly: System.Reflection.AssemblyProductAttribute("12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
